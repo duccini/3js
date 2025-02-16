f486242e228b44750a09b7a4ee8845190cab5eb1
duccini <guilhermeduccini@gmail.com>
2025-02-16 18:37:34 -0300

    create a basic scene

--- ThreeJS_Journey/03/exercise/script.js
+++ ThreeJS_Journey/03/exercise/script.js
@@ -1,3 +1,32 @@
 import * as THREE from "three";
 
-console.log(THREE);
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// 1 Scene
+const scene = new THREE.Scene();
+
+// 2 Objects
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xfaebd7 });
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+scene.add(cubeMesh);
+
+// 3 Camera
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// 4 Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
